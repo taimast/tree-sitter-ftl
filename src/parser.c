ceb7354bb0351768fa227d545e884a4163dcfb52
taimast <charlskenno@gmail.com>
2024-09-21 00:49:29 +0300

    refactor(parser): rename 'fluent' to 'ftl' for consistency

--- src/parser.c
+++ src/parser.c
@@ -787,7 +787,7 @@ extern "C" {
 #define TS_PUBLIC __attribute__((visibility("default")))
 #endif
 
-TS_PUBLIC const TSLanguage *tree_sitter_fluent(void) {
+TS_PUBLIC const TSLanguage *tree_sitter_ftl(void) {
   static const TSLanguage language = {
     .version = LANGUAGE_VERSION,
     .symbol_count = SYMBOL_COUNT,
